d724b3296baee007995930a2bf8a932650c75216
torte478 <torte478@gmail.com>
2022-02-21 09:30:15 +0300

    ftr(*): add new game template

---
+++ parallelWorlds/src/scenes/Game.js
@@ -0,0 +1,29 @@
+import Phaser from '../lib/phaser.js';
+
+import Core from '../game/Core.js';
+
+export default class Game extends Phaser.Scene {
+
+	/** @type {Core} */
+	_core;
+
+    constructor() {
+        super('game');
+    }
+
+    preload() {
+        const me = this;
+    }
+
+    create() {
+        const me = this;
+
+        me._core = new Core(me.add);
+
+        console.log('Hello, world!');
+    }
+
+    update() {
+        const me = this;
+    }
+}
