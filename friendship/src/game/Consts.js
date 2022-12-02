c25fd88ffcc5ba93405c553c880f1a5284547d4a
torte478 <torte478@gmail.com>
2022-12-02 17:17:27 +0300

    ftr(friendship): start new game

---
+++ friendship/src/game/Consts.js
@@ -0,0 +1,13 @@
+export default class Consts {
+
+    static Viewport = {
+        Width: 1000,
+        Height: 800
+    };
+
+    static Unit = 50;
+
+    static Depth = {
+        Max: 100000
+    };
+}
---
+++ friendship/src/scenes/Game.js
@@ -0,0 +1,38 @@
+import Phaser from '../lib/phaser.js';
+
+import Animation from '../game/utils/Animation.js';
+import Utils from '../game/utils/Utils.js';
+
+import Core from '../game/Core.js';
+
+export default class Game extends Phaser.Scene {
+
+	/** @type {Core} */
+	_core;
+
+    constructor() {
+        super('game');
+    }
+
+    preload() {
+        const me = this;
+
+        Utils.runLoadingBar(this);
+
+        Utils.loadWav(me, 'button_click');
+    }
+
+    create() {
+        const me = this;
+
+        Animation.init(me);
+
+        me._core = new Core(me);
+    }
+
+    update() {
+        const me = this;
+
+        me._core.update();
+    }
+}
