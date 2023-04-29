bd5061efdeac3fd07ad443c8f4a4348ba813a50c
torte478 <torte478@gmail.com>
2023-04-29 09:47:28 +0300

    ftr(semaphore): start new game

---
+++ semaphore/src/game/Game.js
@@ -0,0 +1,43 @@
+import Phaser from '../lib/phaser.js';
+
+import Here from '../framework/Here.js';
+import Utils from '../framework/Utils.js';
+
+import Config from './Config.js';
+import Consts from './Consts.js';
+import Enums from './Enums.js';
+
+export default class Game {
+
+    /** @type {Phaser.GameObjects.Text} */
+    _log;
+
+    constructor() {
+        const me = this;
+
+        Utils.debugLog('PAST YOUR CODE HERE!');
+
+        Utils.ifDebug(Config.Debug.ShowSceneLog, () => {
+            me._log = Here._.add.text(10, 10, '', { fontSize: 14, backgroundColor: '#000' })
+                .setScrollFactor(0)
+                .setDepth(Consts.Depth.Max);
+        });
+    }
+
+    update() {
+        const me = this;
+
+        if (Here.Controls.isPressed(Enums.Keyboard.RESTART) 
+            && Utils.isDebug(Config.Debug.Global))
+            Here._.scene.restart({ isRestart: true });
+
+        Utils.ifDebug(Config.Debug.ShowSceneLog, () => {
+            const mouse = Here._.input.activePointer;
+
+            let text = 
+                `mse: ${mouse.worldX | 0} ${mouse.worldY | 0}\n`;
+
+            me._log.setText(text);
+        });
+    }
+}
